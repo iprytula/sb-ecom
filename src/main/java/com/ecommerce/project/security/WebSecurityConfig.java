a60f7f5ba55b35dafaf2bb57ae99a1fd9c6f4ca2
Iurii Prytula <iurii.prytula@gmail.com>
2025-03-17 14:07:36 +0100

    added springdoc-openapi, deleted user initialization on every app start

--- src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -1,22 +1,13 @@
 package com.ecommerce.project.security;
 
-import com.ecommerce.project.config.AppRoles;
-import com.ecommerce.project.model.Role;
-import com.ecommerce.project.model.User;
-import com.ecommerce.project.repository.RoleRepository;
-import com.ecommerce.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -30,8 +21,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.ecommerce.project.security.jwt.AuthEntryPointJwt;
 import com.ecommerce.project.security.jwt.AuthTokenFilter;
 import com.ecommerce.project.security.services.UserDetailsServiceImpl;
-
-import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -109,64 +98,4 @@ public class WebSecurityConfig {
 			"/webjars/**"));
 	}
 
-	@Bean
-	public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		return args -> {
-			// Retrieve or create roles
-			Role userRole = roleRepository.findByRoleName(AppRoles.ROLE_USER)
-				.orElseGet(() -> {
-					Role newUserRole = new Role(AppRoles.ROLE_USER);
-					return roleRepository.save(newUserRole);
-				});
-
-			Role sellerRole = roleRepository.findByRoleName(AppRoles.ROLE_SELLER)
-				.orElseGet(() -> {
-					Role newSellerRole = new Role(AppRoles.ROLE_SELLER);
-					return roleRepository.save(newSellerRole);
-				});
-
-			Role adminRole = roleRepository.findByRoleName(AppRoles.ROLE_ADMIN)
-				.orElseGet(() -> {
-					Role newAdminRole = new Role(AppRoles.ROLE_ADMIN);
-					return roleRepository.save(newAdminRole);
-				});
-
-			Set<Role> userRoles = Set.of(userRole);
-			Set<Role> sellerRoles = Set.of(sellerRole);
-			Set<Role> adminRoles = Set.of(userRole, sellerRole, adminRole);
-
-
-			// Create users if not already present
-			if (!userRepository.existsByUserName("user1")) {
-				User user1 = new User("user1", "user1@example.com", passwordEncoder.encode("password1"));
-				userRepository.save(user1);
-			}
-
-			if (!userRepository.existsByUserName("seller1")) {
-				User seller1 = new User("seller1", "seller1@example.com", passwordEncoder.encode("password2"));
-				userRepository.save(seller1);
-			}
-
-			if (!userRepository.existsByUserName("admin")) {
-				User admin = new User("admin", "admin@example.com", passwordEncoder.encode("adminPass"));
-				userRepository.save(admin);
-			}
-
-			// Update roles for existing users
-			userRepository.findByUserName("user1").ifPresent(user -> {
-				user.setRoles(userRoles);
-				userRepository.save(user);
-			});
-
-			userRepository.findByUserName("seller1").ifPresent(seller -> {
-				seller.setRoles(sellerRoles);
-				userRepository.save(seller);
-			});
-
-			userRepository.findByUserName("admin").ifPresent(admin -> {
-				admin.setRoles(adminRoles);
-				userRepository.save(admin);
-			});
-		};
-	}
 }
